f97729885d3c52cdea0e543fe3b3adee06f1fa75
hanschiu101 <hanschiu101@gmail.com>
2026-03-07 16:39:10 +0800

    新增 prj02.py 檔案，包含 GUI 功能及清除按鈕

--- class1/prj02.py
+++ class1/prj02.py
@@ -6,6 +6,12 @@ from tkinter import *
 def hi_fun():
     # 顯示"Hi SinguLar"在終端機
     print("Hi SinguLar")
+    display.config(text="Hi Singular", fg="red", bg="black")
+
+
+def clear_fun():
+    # 清除標籤及終端機上的文字,前景顏色為白色,背景顏色為白色
+    display.config(text="", fg="white", bg="white")
 
 
 #######################建立視窗########################
@@ -15,13 +21,17 @@ windows = Tk()
 windows.title("My first GUI")
 #######################建立按鈕########################
 # 創建一個按鈕，當被按下時，就會執行hi_fun()函數
-btn1 = Button(windows, text="Hi SinguLar", command=hi_fun)
-# 將按鈕煮煮視窗中
+btn1 = Button(windows, text="screen", command=hi_fun)
+# 將按鈕加入視窗中
 btn1.pack()
+# 創建一個按鈕，當被按下時，就會執行clear_fun函數
+btn2 = Button(windows, text="Clear", command=clear_fun)
+# 將按鈕加入視窗中
+btn2.pack()
 #######################建立標籤########################
 # 創建一個標籤，顯示"Hi Singular"，前景顏色為紅色，背景顏色為黑色
 # Labl參數說明:(視窗名稱，文字內容，前景顏色，背景顏色)
-display = Label(windows, text="Hi Singular", fg="red", bg="black")
+display = Label(windows, text="")
 
 # 將標籤家入主視窗中
 display.pack()
