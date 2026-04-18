686222e5cddd70aab86af39a827a84967da00729
hanschiu101 <hanschiu101@gmail.com>
2026-04-18 16:40:07 +0800

    新增 prj01.py 和 prj02.py，實作 GUI 功能，包括視窗、標籤及按鈕；新增 prj03.py 檔案

---
+++ class5/prj01.py
@@ -0,0 +1,38 @@
+#######################匯入模組#######################
+from ttkbootstrap import *
+import sys
+import os
+
+#######################建立視窗########################
+window = Tk()
+window.title("My  GUI")
+#######################設定字型########################
+font_size = 20
+window.option_add("*Font", ("Helvetica", font_size))
+#######################設定主題########################
+style = Style(theme="minty")
+# "my.TButton"的命名邏輯
+# 就像幫東西貼標籤一樣,分成兩個部分,用[]關開:
+# 前半段"my"->自己取的名字,可以換成任何你想要的名字,例如"big","red"
+# 後半段"TButton"->固定寫法,代表[按鈕]這種元件
+#                     T是Ttk(異種按鈕公具的縮寫)的縮寫
+#                     就像[T桖]的T一樣,是品牌名稱的開頭
+# 常見元件的後半段寫法:
+# 按鈕->TButton1ul 標籤->TLabel 輸入框->TEntry
+style.configure("my.TButton", font=("Helvetica", font_size))
+#######################建立標籤########################
+label = Label(window, text="Hello World")
+label.grid(row=0, column=0, sticky="E")
+
+
+#######################定義函數########################
+def text():
+    print("Hello World")
+
+
+#######################建立按鈕########################
+button = Button(window, text="瀏覽", command=text, style="my.TButton")
+button.grid(row=0, column=1, sticky="W")
+button2 = Button(window, text="顯示", command=text, style="my.TButton")
+button2.grid(row=1, column=0, columnspan=2, sticky="EW")
+window.mainloop()
